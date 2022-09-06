25b9fc34e5095b544e545a377f00e5a5d3fcd81d
Igor Lesik <ilesik@tachyum.com>
2022-09-05 17:47:34 -0700

    add CMake build

---
+++ src/PafMain.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @brief     PAF application main entry point.
+ * @author    Igor Lesik 2022
+ * @copyright Igor Lesik 2022
+ */
+
+int main(int argc, char* argv[])
+{
+    ConsoleApplication app;
+ 
+    app.addHelpCommand("--help|-h", "Usage:", true);
+    app.addVersionCommand("--version|-v", "PAF version 0.0.1");
+ 
+    //app.addCommand ({ "--foo",
+    //                  "--foo filename",
+    //                  "Performs a foo operation on the given file",
+    //                  [] (const auto& args) { doFoo (args); }});
+ 
+    return app.findAndRunCommand(argc, argv);
+}
